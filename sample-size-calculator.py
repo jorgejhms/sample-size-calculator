4bb2480ce77d293a445dc048202e07e7bc62c63f
aniversarioperu <my.calesis@gmail.com>
2013-10-07 13:53:19 +0300

    fixing typo and style

--- sample-size-calculator.py
+++ sample-size-calculator.py
@@ -19,7 +19,7 @@ def p_sample(N, p, z, E):
 	return n
 
 ###Arguments definition###
-parser = argparse.ArgumentParser(prog="Sample size calculator", description="Calcule the apropiate size for a sample.")
+parser = argparse.ArgumentParser(prog="Sample size calculator", description="Calcule the appropiate size for a sample.")
 parser.add_argument("Universe", default=0, type=int, help="Size of the Universe to sample")
 parser.add_argument("Error", default=0, type=float, help="Error margin desired")
 parser.add_argument("Confidence", default=0, type=int, help="Confidence degree desired")
@@ -29,13 +29,13 @@ args = parser.parse_args()
 
 	
 ###Variable declaration###
-N=args.Universe
-n=0
-p=args.Proportion
-E=args.Error
-z=args.Confidence
+N = args.Universe
+n = 0
+p = args.Proportion
+E = args.Error
+z = args.Confidence
 	
-p_sample (N, p, z, E)
+p_sample(N, p, z, E)
 
 ###Data presentation###
 print "Universe is %r" % N
